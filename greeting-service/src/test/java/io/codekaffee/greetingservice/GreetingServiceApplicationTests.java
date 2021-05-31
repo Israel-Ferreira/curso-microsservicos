ffb805807d56a3cb472974f31b077f2cb5f4df45
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2021-05-31 18:05:39 -0300

    Primeiro Microsserviço

---
+++ greeting-service/src/test/java/io/codekaffee/greetingservice/GreetingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codekaffee.greetingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
