f54fe13f14cd0dec5f0000ecc1282114af176122
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2021-06-01 10:25:32 -0300

    Application config in yml file

--- greeting-service/src/main/java/io/codekaffee/greetingservice/controllers/GreetingController.java
+++ greeting-service/src/main/java/io/codekaffee/greetingservice/controllers/GreetingController.java
@@ -1,6 +1,9 @@
 package io.codekaffee.greetingservice.controllers;
 
+import io.codekaffee.greetingservice.config.GreetingConfiguration;
 import io.codekaffee.greetingservice.models.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +13,22 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "%s, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+
+    @Autowired
+    private GreetingConfiguration greetingConfiguration;
+
+
     @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "world") String name){
-        return  new Greeting(counter.incrementAndGet(), String.format(template, name));
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "") String name){
+
+        if(name.isEmpty()){
+            name  = greetingConfiguration.getDefaultValue();
+        }
+
+        return  new Greeting(counter.incrementAndGet(), String.format(template, greetingConfiguration.getGreeting(),name));
     }
 
 }
