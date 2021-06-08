b9e7b3c12219d869c06a5f9b5ed522bbf25d26cb
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2021-06-08 13:18:00 -0300

    Criando o projeto cambio service

--- greeting-service/src/main/java/io/codekaffee/greetingservice/config/GreetingConfiguration.java
+++ greeting-service/src/main/java/io/codekaffee/greetingservice/config/GreetingConfiguration.java
@@ -1,10 +1,12 @@
 package io.codekaffee.greetingservice.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Component
+@RefreshScope
 @ConfigurationProperties("greeting-service")
 public class GreetingConfiguration {
 
