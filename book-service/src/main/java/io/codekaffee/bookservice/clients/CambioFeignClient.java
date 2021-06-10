a40edbc94a3f6aa302574e7c0fd2d03d08b504df
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2021-06-10 17:57:19 -0300

    Testing load balancing and Feign

--- book-service/src/main/java/io/codekaffee/bookservice/clients/CambioFeignClient.java
+++ book-service/src/main/java/io/codekaffee/bookservice/clients/CambioFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "cambio-service", url = "localhost:8004")
+@FeignClient(name = "cambio-service")
 public interface CambioFeignClient {
 
     @GetMapping("/cambio-service/convert")
