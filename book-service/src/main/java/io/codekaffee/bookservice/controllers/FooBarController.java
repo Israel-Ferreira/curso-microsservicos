38d98af925cb822e94f72ce15953f7251b690a69
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2021-06-11 16:46:53 -0300

    Retry

--- book-service/src/main/java/io/codekaffee/bookservice/controllers/FooBarController.java
+++ book-service/src/main/java/io/codekaffee/bookservice/controllers/FooBarController.java
@@ -4,14 +4,29 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
 @RestController
 @RequestMapping("book-service")
 public class FooBarController {
     
 
     @GetMapping("/foo-bar")
+    @Retry(name = "default")
     public ResponseEntity<String> fooBar(){
-        return ResponseEntity.ok("Hello, This processes of drugstore is a Varzea");
+
+        log.info("Foo bar request is received");
+        log.info("Tentando outra Vez...");
+
+        var response = new RestTemplate()
+            .getForEntity("http://localhost:29222", String.class);
+
+
+        return response;
     } 
 }
