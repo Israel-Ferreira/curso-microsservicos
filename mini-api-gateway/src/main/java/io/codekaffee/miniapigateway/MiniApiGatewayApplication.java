b834c0ddf031e840ed1c65db764cab4d896f83d1
Israel-Ferreira <israel.souza5@fatec.sp.gov.br>
2021-06-11 09:50:26 -0300

    Create Project Mini Api-Gateway

---
+++ mini-api-gateway/src/main/java/io/codekaffee/miniapigateway/MiniApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package io.codekaffee.miniapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniApiGatewayApplication.class, args);
+	}
+
+}
---
+++ mini-api-gateway/src/test/java/io/codekaffee/miniapigateway/MiniApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codekaffee.miniapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
